3fc1500ce124955e8dd62863ade551acc0303266
Vangasse <vangasse@ic.ufal.br>
2019-08-20 17:56:10 -0300

    Generate P

--- least_squares.py
+++ least_squares.py
@@ -20,5 +20,17 @@ u = np.random.uniform(low=-1, high=1, size=N)
 #y = [0, 0, 0]
 y = [0]*ny
 
+#geração de saída
 for k in range(ny, N):
     y.append(-0.5*y[k-1] - 0.3*y[k-2] + 0.09*y[k-3] + 8.3*u[k-1] + 1.7*u[k-2] - 5.2*u[k-3])
+
+#aplicação de ruído
+noisy_y = y + noise
+
+#geração da matriz P
+j = nu
+P = []
+for i in range(ny, 10):
+    line = [y[i], y[i-1], y[i-2], u[j], u[j-1], u[j-2]]
+    j += 1
+    P.append(line)

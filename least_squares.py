746166f62d9bfb5f9a78cba64d6086db96195db3
Vangasse <vangasse@ic.ufal.br>
2019-08-24 20:44:09 -0300

    NumPy y

--- least_squares.py
+++ least_squares.py
@@ -7,7 +7,7 @@ Created on Tue Aug 20 15:03:34 2019
 
 import numpy as np
 
-N = 10
+N = 5
 ny = 3
 nu = 3
 
@@ -18,11 +18,13 @@ noise = np.random.uniform(low=-0.2, high=0.2, size=N)
 u = np.random.uniform(low=-1, high=1, size=N)
 
 #y = [0, 0, 0]
-y = [0]*ny
+y = np.empty((0,1))
+y = np.append(y, ny*[0])
+
 
 #geração de saída
 for k in range(ny, N):
-    y.append(-0.5*y[k-1] - 0.3*y[k-2] + 0.09*y[k-3] + 8.3*u[k-1] + 1.7*u[k-2] - 5.2*u[k-3])
+    y = np.append(y, -0.5*y[k-1] - 0.3*y[k-2] + 0.09*y[k-3] + 8.3*u[k-1] + 1.7*u[k-2] - 5.2*u[k-3])
 
 #aplicação de ruído
 noisy_y = y + noise
@@ -37,8 +39,10 @@ for i in range(ny-1, N):
 
 P_transpose = P.transpose()
 
+print (P)
+print (P_transpose)
 
-
+print (P_transpose.dot(P))
 
 
 

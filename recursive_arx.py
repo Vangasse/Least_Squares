4b1180125a99453813dfbb10feb08e7d9f369942
Vangasse <vangasse@ic.ufal.br>
2019-09-09 11:23:34 -0300

    Recursive ARX Fixed and Working

--- recursive_arx.py
+++ recursive_arx.py
@@ -50,56 +50,25 @@ def recursive_arx(u, y, n, l):
     ###################### Setting Parameters ######################
     #Regressor Matrix
     phi = genRegMatrix(u, y, n)
-    #print (phi)
     #Covariance Matrix
     P = np.identity(n*2)*10000
     #System Parameters
-    theta_est = np.array([0.5916, 0.3326, 0.8531, 0.4424, 0.9044, 0.0332]).reshape(2*n,1)
-    #print(theta_est.shape)
-    #theta_est = np.random.uniform(low=-1, high=1, size=(n*2, 1))
+    theta_est = np.random.uniform(low=-1, high=1, size=(n*2, 1))
     ###################### Recursive Procedure ######################
     """N-n"""
-    for i in range(3):
+    for i in range(N-n):
         row_phi = phi[i,:].reshape(1, 2*n)
         row_phi_transpose = row_phi.transpose()
 
         den_K = row_phi.dot(P).dot(row_phi_transpose) + l
         num_K = P.dot(row_phi_transpose)
         K = num_K / den_K
-        #print(K)
-        #time.sleep(2)
 
         theta_est = theta_est + K*(y[i+n] - row_phi.dot(theta_est))
-        #print(theta_est)
-        #time.sleep(2)
         den_P = ((row_phi.dot(P)).dot(row_phi_transpose)) + l
-        num_P = P - ((P.dot(row_phi_transpose)).dot(row_phi)).dot(P)
-        print((den_P))
-        P = (1/l)*(num_P/den_P)
-        #print(P)
-        #time.sleep(10)
-    """phi_t = phi.transpose()
-    phi_pseudo_inverse = (np.linalg.inv(phi_t.dot(phi))).dot(phi_t)
-    theta_est = phi_pseudo_inverse.dot(y[n:])
+        num_P = ((P.dot(row_phi_transpose)).dot(row_phi)).dot(P)
+        P = (1/l)*(P - (num_P/den_P))
     
-    ###################### Model Evaluation ######################
-    y_est = phi.dot(theta_est)
-    y_est = np.append(y[0:n], y_est, axis=0)
-    
-    y_mean = np.mean(y_est)
-    erro = y - y_est
-    
-    MSE=0
-    for i in range(len(erro)):
-        MSE += erro[i] ** 2
-    C=0
-    for k in range(len(y)):
-        C += (y[k] - y_mean) ** 2
-    COEEF = 1 - MSE/C
-    
-    print("MSE: "+str(MSE))
-    print("COEEF: "+str(COEEF))
-    """
     return theta_est
 
 
@@ -120,4 +89,4 @@ if DEBUG:
 
 ###################### Resulting Array of Parameters ######################
 theta = recursive_arx(u,y,n, 0.97)#lambda = 0.97
-#print(theta)
+print(theta)
